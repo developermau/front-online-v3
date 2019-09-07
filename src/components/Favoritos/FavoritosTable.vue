b6999b1e0523652aea752e623286aef86a37167c
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-07 16:49:26 -0400

    ! template de tabla con filtros y crud

--- src/components/Favoritos/FavoritosTable.vue
+++ src/components/Favoritos/FavoritosTable.vue
@@ -1,115 +1,249 @@
 <template>
-  <v-card>
-    <v-card-title>
-      Nutrition
-      <div class="flex-grow-1"></div>
-      <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
-    </v-card-title>
-    <v-data-table :headers="headers" :items="desserts" :search="search"></v-data-table>
-  </v-card>
+  <v-data-table :headers="headers" :items="desserts" :search="search">
+    <template v-slot:top>
+      <v-toolbar flat color="white">
+        <v-toolbar-title>My CRUD</v-toolbar-title>
+        <v-divider class="mx-4" inset vertical></v-divider>
+        <div class="flex-grow-1"></div>
+        <v-dialog v-model="dialog" max-width="500px">
+          <template v-slot:activator="{ on }">
+            <v-btn color="primary" dark class="mb-2" v-on="on">New Item</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
+
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
+                  </v-col>
+                  <v-col cols="12" sm="6" md="4">
+                    <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+
+            <v-card-actions>
+              <div class="flex-grow-1"></div>
+              <v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
+    <template v-slot:body.append>
+      <tr>
+        <td></td>
+        <td>
+          <v-text-field v-model="calories" type="number" label="Less than"></v-text-field>
+        </td>
+        <td>
+          <v-text-field v-model="fat" type="number" label="Less than"></v-text-field>
+        </td>
+        <td colspan="3"></td>
+      </tr>
+    </template>
+    <template v-slot:item.action="{ item }">
+      <v-icon small class="mr-2" @click="editItem(item)">edit</v-icon>
+      <v-icon small @click="deleteItem(item)">delete</v-icon>
+    </template>
+    <template v-slot:no-data>
+      <v-btn color="primary" @click="initialize">Reset</v-btn>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      dialog: false,
       search: "",
-      headers: [
+      // Filters
+      calories: "",
+      fat: "",
+      desserts: [],
+      editedIndex: -1,
+      editedItem: {
+        name: "",
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0
+      },
+      defaultItem: {
+        name: "",
+        calories: 0,
+        fat: 0,
+        carbs: 0,
+        protein: 0
+      }
+    };
+  },
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? "New Item" : "Edit Item";
+    },
+    headers() {
+      return [
         {
           text: "Dessert (100g serving)",
           align: "left",
           sortable: false,
           value: "name"
         },
-        { text: "Calories", value: "calories" },
-        { text: "Fat (g)", value: "fat" },
+        {
+          text: "Calories",
+          value: "calories",
+          filter: value => {
+            if (!this.calories) return true;
+
+            return value < parseInt(this.calories);
+          }
+        },
+        {
+          text: "Fat (g)",
+          value: "fat",
+          filter: value => {
+            if (!this.fat) return true;
+
+            return value < parseInt(this.fat);
+          }
+        },
         { text: "Carbs (g)", value: "carbs" },
         { text: "Protein (g)", value: "protein" },
         { text: "Iron (%)", value: "iron" }
-      ],
-      desserts: [
+      ];
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close();
+    }
+  },
+  created() {
+    this.initialize();
+  },
+
+  methods: {
+    initialize() {
+      this.desserts = [
         {
           name: "Frozen Yogurt",
           calories: 159,
           fat: 6.0,
           carbs: 24,
-          protein: 4.0,
-          iron: "1%"
+          protein: 4.0
         },
         {
           name: "Ice cream sandwich",
           calories: 237,
           fat: 9.0,
           carbs: 37,
-          protein: 4.3,
-          iron: "1%"
+          protein: 4.3
         },
         {
           name: "Eclair",
           calories: 262,
           fat: 16.0,
           carbs: 23,
-          protein: 6.0,
-          iron: "7%"
+          protein: 6.0
         },
         {
           name: "Cupcake",
           calories: 305,
           fat: 3.7,
           carbs: 67,
-          protein: 4.3,
-          iron: "8%"
+          protein: 4.3
         },
         {
           name: "Gingerbread",
           calories: 356,
           fat: 16.0,
           carbs: 49,
-          protein: 3.9,
-          iron: "16%"
+          protein: 3.9
         },
         {
           name: "Jelly bean",
           calories: 375,
           fat: 0.0,
           carbs: 94,
-          protein: 0.0,
-          iron: "0%"
+          protein: 0.0
         },
         {
           name: "Lollipop",
           calories: 392,
           fat: 0.2,
           carbs: 98,
-          protein: 0,
-          iron: "2%"
+          protein: 0
         },
         {
           name: "Honeycomb",
           calories: 408,
           fat: 3.2,
           carbs: 87,
-          protein: 6.5,
-          iron: "45%"
+          protein: 6.5
         },
         {
           name: "Donut",
           calories: 452,
           fat: 25.0,
           carbs: 51,
-          protein: 4.9,
-          iron: "22%"
+          protein: 4.9
         },
         {
           name: "KitKat",
           calories: 518,
           fat: 26.0,
           carbs: 65,
-          protein: 7,
-          iron: "6%"
+          protein: 7
         }
-      ]
-    };
+      ];
+    },
+
+    editItem(item) {
+      this.editedIndex = this.desserts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+    deleteItem(item) {
+      const index = this.desserts.indexOf(item);
+      confirm("Are you sure you want to delete this item?") &&
+        this.desserts.splice(index, 1);
+    },
+
+    close() {
+      this.dialog = false;
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+      }, 300);
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+      } else {
+        this.desserts.push(this.editedItem);
+      }
+      this.close();
+    }
   }
 };
 </script>
