375957952e841bc5929c1edf474b46c2faa2a333
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-08 16:01:37 -0400

    * organizar los iconos

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,10 @@
   <section id="About">
     <v-btn text href="https://materialdesignicons.com/">Mas iconos</v-btn>
     <v-container>
-      <span v-for="(iconName, index) in iconList" :key="index">
-        <v-icon>{{index}}. {{iconName}}</v-icon> |
+      <span v-for="(icon, index) in iconList" :key="index">
+        <v-icon>{{index}}. {{icon.name}}</v-icon>
+        {{icon.name}}
+        <v-spacer></v-spacer>
       </span>
     </v-container>
   </section>
@@ -23,27 +25,27 @@ export default {
   methods: {
     fetchIcons() {
       this.iconList = [
-        "mdi-eye",
-        "mdi-eye-off",
-        "mdi-anchor",
-        "mdi-xbox-controller",
-        "mdi-watch",
-        "mdi-tilde",
-        "mdi-tennis",
-        "mdi-mouse",
-        "mdi-pencil",
-        "mdi-delete",
-        "mdi-eye",
-        "mdi-heart",
-        "mdi-cart",
-        "mdi-cart-remove",
-        "mdi-email",
-        "mdi-cellphone",
-        "mdi-laptop",
-        "mdi-phone",
-        "mdi-television-classic",
-        "mdi-magnify",
-        "mdi-camera-plus"
+        { name: "mdi-eye" },
+        { name: "mdi-eye-off" },
+        { name: "mdi-anchor" },
+        { name: "mdi-xbox-controller" },
+        { name: "mdi-watch" },
+        { name: "mdi-tilde" },
+        { name: "mdi-tennis" },
+        { name: "mdi-mouse" },
+        { name: "mdi-pencil" },
+        { name: "mdi-delete" },
+        { name: "mdi-eye" },
+        { name: "mdi-heart" },
+        { name: "mdi-cart" },
+        { name: "mdi-cart-remove" },
+        { name: "mdi-email" },
+        { name: "mdi-cellphone" },
+        { name: "mdi-laptop" },
+        { name: "mdi-phone" },
+        { name: "mdi-television-classic" },
+        { name: "mdi-magnify" },
+        { name: "mdi-camera-plus" }
       ];
     }
   }
