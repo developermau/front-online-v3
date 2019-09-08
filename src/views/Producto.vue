fc614e96d7f8ebc587e2e0ee6c90bd9a6bef6fb5
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-08 17:22:33 -0400

    Trabajando en la parte visual de las fotografias

--- src/views/Producto.vue
+++ src/views/Producto.vue
@@ -12,6 +12,54 @@
             <v-card-text class="text-center">
               <v-icon size="200" color="primary" @click="addFotografia">mdi-camera-plus</v-icon>
             </v-card-text>
+            <v-card-actions>
+              <!-- DIALOG: Fotografia -->
+              <v-dialog v-model="dialogFotografia" max-width="500px" persistent>
+                <template v-slot:activator="{ on }">
+                  <v-btn block color="primary" dark v-on="on">Nueva Fotografia</v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">Subir fotografias</span>
+                  </v-card-title>
+
+                  <v-card-text>
+                    <v-file-input
+                      v-model="files"
+                      color="deep-purple accent-4"
+                      counter
+                      label="File input"
+                      multiple
+                      placeholder="Select your files"
+                      prepend-icon="mdi-paperclip"
+                      outlined
+                      :show-size="1000"
+                    >
+                      <template v-slot:selection="{ index, text }">
+                        <v-chip
+                          v-if="index < 2"
+                          color="deep-purple accent-4"
+                          dark
+                          label
+                          small
+                        >{{ text }}</v-chip>
+
+                        <span
+                          v-else-if="index === 2"
+                          class="overline grey--text text--darken-3 mx-2"
+                        >+{{ files.length - 2 }} File(s)</span>
+                      </template>
+                    </v-file-input>
+                  </v-card-text>
+
+                  <v-card-actions>
+                    <div class="flex-grow-1"></div>
+                    <v-btn color="blue darken-1" text @click="dialogFotografia = false">Cancelar</v-btn>
+                    <v-btn color="blue darken-1" text @click="uploadFiles">Subir</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-card-actions>
           </v-card>
         </v-col>
         <v-col cols="5">
@@ -37,7 +85,9 @@ export default {
   components: { FotografiaCarrusel, ProductoCardDetails },
   data: function() {
     return {
-      producto: {}
+      producto: {},
+      dialogFotografia: false,
+      files: []
     };
   },
   async beforeRouteEnter(to, from, next) {
@@ -57,6 +107,10 @@ export default {
   methods: {
     addFotografia() {
       console.log("Adicionando fotografias...para el producto", this.producto);
+    },
+    uploadFiles() {
+      console.log("uploading....", this.files);
+      this.dialogFotografia = false;
     }
   }
 };
