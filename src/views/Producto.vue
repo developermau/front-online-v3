6da5a54e88b0f52522c8ed5036c1e1dfc6632333
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 10:55:37 -0400

    * Version estable upload files

--- src/views/Producto.vue
+++ src/views/Producto.vue
@@ -25,37 +25,22 @@
 
                   <v-card-text>
                     <v-file-input
-                      v-model="files"
-                      color="deep-purple accent-4"
-                      counter
-                      label="File input"
+                      v-model="fotografias"
+                      id="fotografias"
+                      name="fotografias"
+                      accept="image/png, image/jpeg, image/bmp"
                       multiple
-                      placeholder="Select your files"
-                      prepend-icon="mdi-paperclip"
-                      outlined
-                      :show-size="1000"
-                    >
-                      <template v-slot:selection="{ index, text }">
-                        <v-chip
-                          v-if="index < 2"
-                          color="deep-purple accent-4"
-                          dark
-                          label
-                          small
-                        >{{ text }}</v-chip>
-
-                        <span
-                          v-else-if="index === 2"
-                          class="overline grey--text text--darken-3 mx-2"
-                        >+{{ files.length - 2 }} File(s)</span>
-                      </template>
-                    </v-file-input>
+                      chips
+                      prepend-icon="mdi-camera"
+                      color="secondary"
+                      label="Select Photos to Upload"
+                    />
                   </v-card-text>
 
                   <v-card-actions>
                     <div class="flex-grow-1"></div>
                     <v-btn color="blue darken-1" text @click="dialogFotografia = false">Cancelar</v-btn>
-                    <v-btn color="blue darken-1" text @click="uploadFiles">Subir</v-btn>
+                    <v-btn color="blue darken-1" text @click="prepararArchivos">Subir</v-btn>
                   </v-card-actions>
                 </v-card>
               </v-dialog>
@@ -88,7 +73,7 @@ export default {
     return {
       producto: {},
       dialogFotografia: false,
-      files: []
+      fotografias: []
     };
   },
   async beforeRouteEnter(to, from, next) {
@@ -109,8 +94,25 @@ export default {
     addFotografia() {
       console.log("Adicionando fotografias...para el producto", this.producto);
     },
-    uploadFiles() {
-      console.log("uploading....", this.files);
+    prepararArchivos(event) {
+      console.log("preparing....", this.fotografias);
+
+      if (this.fotografias.length > 0) {
+        let formData = new FormData();
+        this.fotografias.forEach(fotografia => {
+          formData.append("fotografias", fotografia);
+        });
+        this.uploadFilesAsFormDataToServer(formData);
+      }
+    },
+    async uploadFilesAsFormDataToServer(formData) {
+      const section = "fotografias";
+      const { data } = await UploadsRepository.uploadFile(section, formData);
+  
+      console.log(data);
+  
+
+      // Close Dialog
       this.dialogFotografia = false;
     }
   }
