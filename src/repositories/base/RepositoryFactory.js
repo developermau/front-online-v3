837d83d4694db6275544702b1d16af1448d04903
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 17:33:57 -0400

    + Declarar FotografiasUploadsRepository

--- src/repositories/base/RepositoryFactory.js
+++ src/repositories/base/RepositoryFactory.js
@@ -1,19 +1,23 @@
 import ProductosRepository from "../api/productosRepository";
 import CategoriasRepository from "../api/categoriasRepository";
 import ProveedoresRepository from "../api/proveedoresRepository";
+import FotografiasRepository from "../api/fotografiasRepository";
 // relaciones
 import RelGustaRepository from "../api/relGustaRepository";
 // Uploads
 import UploadsRepository from "../api/uploadsRepository";
+import FotografiasUploadsRepository from "../api/fotografiasUploadsRepository";
 
 const repositories = {
   productos: ProductosRepository,
   categorias: CategoriasRepository,
   proveedores: ProveedoresRepository,
+  fotografias: FotografiasRepository,
   // relaciones
   gustas: RelGustaRepository,
   // Uploads
-  uploads: UploadsRepository
+  uploads: UploadsRepository,
+  uploadFotografias: FotografiasUploadsRepository
   // other repositories ...
 };
 
