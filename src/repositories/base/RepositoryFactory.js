90d6bcf8a3cdc78bcddd36b14c3d05618f002a58
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 10:28:27 -0400

    * registrar el repository para el upload

--- src/repositories/base/RepositoryFactory.js
+++ src/repositories/base/RepositoryFactory.js
@@ -3,13 +3,17 @@ import CategoriasRepository from "../api/categoriasRepository";
 import ProveedoresRepository from "../api/proveedoresRepository";
 // relaciones
 import RelGustaRepository from "../api/relGustaRepository";
+// Uploads
+import UploadsRepository from "../api/uploadsRepository";
 
 const repositories = {
   productos: ProductosRepository,
   categorias: CategoriasRepository,
   proveedores: ProveedoresRepository,
   // relaciones
-  gustas: RelGustaRepository
+  gustas: RelGustaRepository,
+  // Uploads
+  uploads: UploadsRepository
   // other repositories ...
 };
 
