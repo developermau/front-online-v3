b71d52e4259936f8a5b3286e606e2d8b630ff531
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-15 13:09:23 -0400

    + Boton añadir a favoritos

--- src/components/Producto/ProductoCardDetails.vue
+++ src/components/Producto/ProductoCardDetails.vue
@@ -4,11 +4,17 @@
     <v-card-text>{{producto.pr_descripcion}}</v-card-text>
     <v-card-actions>
       <v-btn dark color="success" @click="addToCart">Añadir al carrito</v-btn>
+      <v-btn dark color="info" @click="addToFavorites">Añadir a favoritos</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+// Repository Factory
+import { RepositoryFactory } from "../../repositories/base/RepositoryFactory";
+// Repositories
+const RelGustaRepository = RepositoryFactory.get("gustas");
+
 export default {
   props: ["producto"],
   name: "ProductoCardDetails",
@@ -19,6 +25,25 @@ export default {
         cantidad: 1
       };
       this.$store.dispatch("cart/addProductToCart", productInCart);
+    },
+    async addToFavorites() {
+      console.log("Añadiendo a favoritos...", this.producto);
+
+      const usuarioId = 2;
+
+      const newRelGusta = {
+        us_usuario: usuarioId,
+        pr_producto: this.producto.pr_producto
+      };
+
+      console.log("newRelGusta", newRelGusta);
+
+      console.log("newRelGusta", newRelGusta);
+      const { data } = await RelGustaRepository.createRelation(newRelGusta);
+      console.log("data", data);
+
+      // Vuex
+      this.$store.dispatch("favorite/addProductToFavoritos", this.producto);
     }
   }
 };
