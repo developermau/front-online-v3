8aa715fd8e1d9da1b6b7008b63a80c837a914a38
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 22:36:38 -0400

    * Datos mas reales para los favoritos

--- src/components/Favoritos/FavoritosDataIterator.vue
+++ src/components/Favoritos/FavoritosDataIterator.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container fluid>
     <v-data-iterator
-      :items="items"
+      :items="favoritos"
       :items-per-page.sync="itemsPerPage"
       :page="page"
       :search="search"
@@ -29,7 +29,7 @@
               hide-details
               :items="keys"
               prepend-inner-icon="mdi-magnify"
-              label="Sort by"
+              label="Ordenar por"
             ></v-select>
             <div class="flex-grow-1"></div>
             <v-btn-toggle v-model="sortDesc" mandatory>
@@ -46,9 +46,9 @@
 
       <template v-slot:default="props">
         <v-row>
-          <v-col v-for="item in props.items" :key="item.name" cols="12" sm="6" md="4" lg="3">
+          <v-col v-for="item in props.items" :key="item.pr_nombre" cols="12" sm="6" md="4" lg="3">
             <v-card>
-              <v-card-title class="subheading font-weight-bold">{{ item.name }}</v-card-title>
+              <v-card-title class="subheading font-weight-bold">{{ item.pr_nombre }}</v-card-title>
 
               <v-divider></v-divider>
 
@@ -69,7 +69,7 @@
 
       <template v-slot:footer>
         <v-row class="mt-2" align="center" justify="center">
-          <span class="grey--text">Items per page</span>
+          <span class="grey--text">Productos por página</span>
           <v-menu offset-y>
             <template v-slot:activator="{ on }">
               <v-btn dark text color="primary" class="ml-2" v-on="on">
@@ -90,7 +90,7 @@
 
           <div class="flex-grow-1"></div>
 
-          <span class="mr-4 grey--text">Page {{ page }} of {{ numberOfPages }}</span>
+          <span class="mr-4 grey--text">Pagina {{ page }} de {{ numberOfPages }}</span>
           <v-btn fab dark color="blue darken-3" class="mr-1" @click="formerPage">
             <v-icon>mdi-chevron-left</v-icon>
           </v-btn>
@@ -105,132 +105,32 @@
 
 <script>
 export default {
+  props: ["favoritos"],
   data() {
     return {
-      itemsPerPageArray: [4, 8, 12],
+      itemsPerPageArray: [3, 6, 9],
       search: "",
       filter: {},
       sortDesc: false,
       page: 1,
-      itemsPerPage: 4,
+      itemsPerPage: 3,
       sortBy: "name",
       keys: [
-        "Name",
-        "Calories",
-        "Fat",
-        "Carbs",
-        "Protein",
-        "Sodium",
-        "Calcium",
-        "Iron"
-      ],
-      items: [
-        {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          sodium: 87,
-          calcium: "14%",
-          iron: "1%"
-        },
-        {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          sodium: 129,
-          calcium: "8%",
-          iron: "1%"
-        },
-        {
-          name: "Eclair",
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          sodium: 337,
-          calcium: "6%",
-          iron: "7%"
-        },
-        {
-          name: "Cupcake",
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          sodium: 413,
-          calcium: "3%",
-          iron: "8%"
-        },
-        {
-          name: "Gingerbread",
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          sodium: 327,
-          calcium: "7%",
-          iron: "16%"
-        },
-        {
-          name: "Jelly bean",
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          sodium: 50,
-          calcium: "0%",
-          iron: "0%"
-        },
-        {
-          name: "Lollipop",
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          sodium: 38,
-          calcium: "0%",
-          iron: "2%"
-        },
-        {
-          name: "Honeycomb",
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          sodium: 562,
-          calcium: "0%",
-          iron: "45%"
-        },
-        {
-          name: "Donut",
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          sodium: 326,
-          calcium: "2%",
-          iron: "22%"
-        },
-        {
-          name: "KitKat",
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          sodium: 54,
-          calcium: "12%",
-          iron: "6%"
-        }
+        "producto",
+        "nombre",
+        "descripcion",
+        "marca",
+        "precio_bs",
+        "precio_envio_bs",
+        "stock",
+        "year",
+        "estado"
       ]
     };
   },
   computed: {
     numberOfPages() {
-      return Math.ceil(this.items.length / this.itemsPerPage);
+      return Math.ceil(this.favoritos.length / this.itemsPerPage);
     },
     filteredKeys() {
       return this.keys.filter(key => key !== `Name`);
--- src/views/Favoritos.vue
+++ src/views/Favoritos.vue
@@ -25,15 +25,54 @@ export default {
     };
   },
   created() {
-    const userId = 1;
+    const userId = 2;
     this.fetchFavoriteProductsByUser(userId);
   },
   methods: {
     async fetchFavoriteProductsByUser(userId) {
       this.isLoading = true;
       const { data } = await RelGustaRepository.getRelationListByUser(userId);
+
+      if (
+        this.favoriteProducts === null &&
+        this.favoriteProducts === undefined
+      ) {
+        this.favoriteProducts = [];
+      }
+
+      if (data !== null && data !== undefined) {
+        data.forEach(item => {
+          console.log(item);
+          console.log(item.producto);
+
+          const producto = item.producto;
+
+          const productoConvertToDataIterator = this.convertProductoToDataIterator(
+            producto
+          );
+
+          this.favoriteProducts.push(productoConvertToDataIterator);
+        });
+      }
+
       this.isLoading = false;
-      this.favoriteProducts = data;
+    },
+    convertProductoToDataIterator(producto) {
+      return {
+        producto: producto.pr_producto,
+        nombre: producto.pr_nombre,
+        descripcion: producto.pr_descripcion,
+        marca: producto.pr_marca,
+        precio_bs: producto.pr_precio_bs,
+        precio_envio_bs: producto.pr_precio_envio_bs,
+        stock: producto.pr_stock,
+        year: producto.pr_year,
+        estado: producto.pr_estado,
+        categoria: producto.ca_categoria,
+        proveedor: producto.pr_proveedor,
+        createdAt: producto.createdAt,
+        updatedAt: producto.updatedAt
+      };
     }
   }
 };
