c7e153522847be759878ae839f1573c3429ca48d
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-14 21:35:44 -0400

    * Subir imagen del producto

--- src/views/Producto.vue
+++ src/views/Producto.vue
@@ -3,49 +3,52 @@
     <v-container fluid>
       <v-row justify="center">
         <v-col cols="7">
-          <v-card v-if="hasFotografias" dark>
+          <v-card>
             <v-card-text class="text-center">
-              <FotografiaCarrusel :fotografias="producto.fotografias" />
+              <div v-if="hasFotografias" dark>
+                <FotografiaCarrusel :fotografias="getFotografias" />
+              </div>
+              <div v-else>
+                <v-icon size="200" color="primary" @click="addFotografia">mdi-camera-plus</v-icon>
+              </div>
             </v-card-text>
+            <v-card-actions>
+              <v-btn block color="primary" dark @click="openDialog">Nueva Fotografia</v-btn>
+            </v-card-actions>
           </v-card>
-          <v-card v-else dark>
+          <!-- <v-card v-else dark>
             <v-card-text class="text-center">
               <v-icon size="200" color="primary" @click="addFotografia">mdi-camera-plus</v-icon>
             </v-card-text>
-            <v-card-actions>
-              <!-- DIALOG: Fotografia -->
-              <v-dialog v-model="dialogFotografia" max-width="500px" persistent>
-                <template v-slot:activator="{ on }">
-                  <v-btn block color="primary" dark v-on="on">Nueva Fotografia</v-btn>
-                </template>
-                <v-card>
-                  <v-card-title>
-                    <span class="headline">Subir fotografias</span>
-                  </v-card-title>
+          </v-card>-->
+          <!-- DIALOG: Fotografia -->
+          <v-dialog v-model="dialogFotografia" max-width="500px" persistent>
+            <v-card>
+              <v-card-title>
+                <span class="headline">Subir fotografias</span>
+              </v-card-title>
 
-                  <v-card-text>
-                    <v-file-input
-                      v-model="fotografias"
-                      id="fotografias"
-                      name="fotografias"
-                      accept="image/png, image/jpeg, image/bmp"
-                      multiple
-                      chips
-                      prepend-icon="mdi-camera"
-                      color="secondary"
-                      label="Select Photos to Upload"
-                    />
-                  </v-card-text>
+              <v-card-text>
+                <v-file-input
+                  v-model="fotografias"
+                  id="fotografias"
+                  name="fotografias"
+                  accept="image/png, image/jpeg, image/bmp"
+                  multiple
+                  chips
+                  prepend-icon="mdi-camera"
+                  color="secondary"
+                  label="Seleccione las fotografias a subir"
+                />
+              </v-card-text>
 
-                  <v-card-actions>
-                    <div class="flex-grow-1"></div>
-                    <v-btn color="blue darken-1" text @click="dialogFotografia = false">Cancelar</v-btn>
-                    <v-btn color="blue darken-1" text @click="prepararArchivos">Subir</v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            </v-card-actions>
-          </v-card>
+              <v-card-actions>
+                <div class="flex-grow-1"></div>
+                <v-btn color="error" @click="dialogFotografia = false">Cancelar</v-btn>
+                <v-btn color="success" @click="prepararArchivos">Subir</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-col>
         <v-col cols="5">
           <ProductoCardDetails :producto="producto" />
@@ -76,11 +79,6 @@ export default {
       fotografias: []
     };
   },
-  watch: {
-    producto: function (newProducto, oldProducto) {
-      console.log(this.hasFotografias);
-    }
-  },
   async beforeRouteEnter(to, from, next) {
     const { data } = await ProductosRepository.getProducto(to.params.id);
     next(vm => (vm.producto = data.data));
@@ -93,9 +91,16 @@ export default {
         this.producto.fotografias.length > 0;
       console.log("hasFotografias", result);
       return result;
+    },
+    getFotografias(){
+      return this.producto.fotografias;
     }
   },
   methods: {
+    openDialog() {
+      this.fotografias = [];
+      this.dialogFotografia = true;
+    },
     addFotografia() {
       console.log("Adicionando fotografias...para el producto", this.producto);
     },
@@ -108,6 +113,8 @@ export default {
           formData.append("fotografias", fotografia);
         });
         this.uploadFilesAsFormDataToServer(formData);
+        // Close Dialog
+        this.dialogFotografia = false;
       }
     },
     async uploadFilesAsFormDataToServer(formData) {
@@ -118,9 +125,20 @@ export default {
         formData
       );
       const fotografias = data.fotografias;
-      this.producto.fotografias = fotografias;
-      // Close Dialog
-      this.dialogFotografia = false;
+      console.log("fotografias", fotografias);
+
+      if (fotografias !== null && fotografias !== undefined) {
+        if (
+          this.producto.fotografias === null &&
+          this.producto.fotografias === undefined
+        ) {
+          this.producto.fotografias = [];
+        }
+
+        fotografias.forEach(fotografia => {
+          this.producto.fotografias.push(fotografia);
+        });
+      }
     }
   }
 };
