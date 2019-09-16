8130931128547db3912c7b9bc8318072607a118e
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-15 20:11:35 -0400

    ! Trabajar en opiniones

--- src/components/Opiniones/OpinionesLista.vue
+++ src/components/Opiniones/OpinionesLista.vue
@@ -1,34 +1,32 @@
 <template>
-  <div>
-    <v-list three-line>
-      <template v-for="(opinion, index) in opiniones" v-key="index">
-        <v-subheader v-if="opinion.header" :key="opinion.header" v-text="opinion.header"></v-subheader>
+  <v-list three-line>
+    <v-list-item v-for="(opinion, index) in opiniones" :key="index">
+      <v-list-item-avatar>
+        <v-img :src="opinion.usuario.us_avatar"></v-img>
+      </v-list-item-avatar>
 
-        <!-- <v-divider v-else-if="opinion.divider" :key="index" :inset="opinion.inset"></v-divider> -->
+      <v-list-item-content>
+        <v-list-item-title v-text="opinion.op_texto"></v-list-item-title>
+        <v-list-item-subtitle v-text="opinion.usuario.us_primer_nombre"></v-list-item-subtitle>
+        <v-rating v-model="opinion.op_calificacion"></v-rating>
+      </v-list-item-content>
 
-        <v-list-item v-else :key="opinion.op_opinion">
-          <v-list-item-avatar>
-            <v-img :src="opinion.usuario.us_avatar"></v-img>
-          </v-list-item-avatar>
-
-          <v-list-item-content>
-            <v-list-item-title>{{opinion.usuario.us_primer_nombre}} {{opinion.usuario.us_segundo_nombre}} {{opinion.usuario.us_paterno_apellido}}</v-list-item-title>
-            <v-list-item-subtitle v-html="opinion.op_texto"></v-list-item-subtitle>
-          </v-list-item-content>
-          <v-list-item-action>
-            <v-rating v-model="opinion.op_calificacion"></v-rating>
-          </v-list-item-action>
-        </v-list-item>
-        <v-divider :key="index"></v-divider>
-      </template>
-    </v-list>
-  </div>
+      <v-list-item-icon>
+        <v-btn>X</v-btn>
+      </v-list-item-icon>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
 export default {
   props: ["opiniones"],
-  name: "OpinionesLista"
+  name: "OpinionesLista",
+  methods: {
+    deleteOpinion(opinionId) {
+      console.log("opinionId", opinionId);
+    }
+  }
 };
 </script>
 
