049dd7eada7414868a97c1ad540a2e171961bea3
Alejandro M. Coca Rojas <alxmcr@outlook.com>
2019-09-15 23:49:47 -0400

    * Mejoras en el repositorio de RelGusta

--- src/repositories/api/relGustaRepository.js
+++ src/repositories/api/relGustaRepository.js
@@ -10,11 +10,22 @@ export default {
     return Repository.get(`${resource}/usuarios/${userId}`);
   },
 
+  getRelationListByProducto(productoId) {
+    return Repository.get(`${resource}/productos/${productoId}`);
+  },
+
   createRelation(payload) {
     return Repository.post(`${resource}`, payload);
   },
 
   deleteProducto(payload) {
+    return Repository.delete(`${resource}`, { data: payload });
+  },
+  deleteRelation(usuarioId, productoId) {
+    const payload = {
+      us_usuario: usuarioId,
+      pr_producto: productoId
+    };
     return Repository.delete(`${resource}`, { data: payload });
   }
 };
